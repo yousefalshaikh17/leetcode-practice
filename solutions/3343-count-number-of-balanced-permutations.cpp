8fa2065a9df82839e0b73ae91ba6af4b81b5a657
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-05-10 20:42:16 +0100

    Fixed code formatting to match LeetCode

--- solutions/3343-count-number-of-balanced-permutations.cpp
+++ solutions/3343-count-number-of-balanced-permutations.cpp
@@ -4,26 +4,25 @@
 
 
 class Solution {
-    public:
-        long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-            // Time: O(n)
-            // Space: O(1)
-            long long numSubarrays = 0;
-            int minPos = -1, maxPos = -1, badPos = -1;
-    
-            for (int i = 0; i < nums.size(); ++i) {
-                int num = nums[i];
-                if (num < minK || num > maxK)
-                    badPos = i;
-                if (num == minK)
-                    minPos = i;
-                if (num == maxK)
-                    maxPos = i;
-    
-                numSubarrays += max(0, min(minPos, maxPos) - badPos);
-            }
-    
-            return numSubarrays;
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        // Time: O(n)
+        // Space: O(1)
+        long long numSubarrays = 0;
+        int minPos = -1, maxPos = -1, badPos = -1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            if (num < minK || num > maxK)
+                badPos = i;
+            if (num == minK)
+                minPos = i;
+            if (num == maxK)
+                maxPos = i;
+
+            numSubarrays += max(0, min(minPos, maxPos) - badPos);
         }
-    };
-    
+
+        return numSubarrays;
+    }
+};
